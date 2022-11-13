aa606e64f632c606ea70291b211bec6a5fd7d222
Chijioke Kemakolam <chijiokekemakolam@gmail.com>
2022-11-13 09:23:27 +0000

    Updated...

--- app/src/main/java/uk/ac/tees/b1325384/easyfood/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1325384/easyfood/MainActivity.java
@@ -2,13 +2,40 @@ package uk.ac.tees.b1325384.easyfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        Button btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Nabila.ttf");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
