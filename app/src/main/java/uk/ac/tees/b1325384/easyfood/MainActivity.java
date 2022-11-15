428de6e3a4d3a688478d100a8581d554a56c72d4
Chijioke Kemakolam <chijiokekemakolam@gmail.com>
2022-11-15 07:14:56 +0000

    Updated...

--- app/src/main/java/uk/ac/tees/b1325384/easyfood/MainActivity.java
+++ app/src/main/java/uk/ac/tees/b1325384/easyfood/MainActivity.java
@@ -2,6 +2,7 @@ package uk.ac.tees.b1325384.easyfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -37,5 +38,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent SignIn = new Intent(MainActivity.this, uk.ac.tees.b1325384.easyfood.SignIn.class);
+                startActivity(SignIn);
+            }
+        });
     }
 }
